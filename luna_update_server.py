16af6f21c43fbeffc487aa059d4a89ee3d042e35
IndonesiaModde <98704611+IndonesiaModde@users.noreply.github.com>
2026-05-10 07:54:08 -0300

    Update luna_update_server.py

--- luna_update_server.py
+++ luna_update_server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # ================================
 # CONFIG
 # ================================
-LATEST_VERSION = "1.17.2"
+LATEST_VERSION = "1.17.1"
 FILEINFO_URL = "https://versionscommon.onrender.com/assets/android/fileinfo"
 
 APK_URL = "https://seuservidor.com/ff.apk"
